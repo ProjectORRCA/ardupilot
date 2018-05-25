5fe7fa936f5ea85fb4d167d50942d1019cfe2c46
ProjectORRCA <39537247+ProjectORRCA@users.noreply.github.com>
2018-05-25 13:48:23 -0400

    Update MODE LAUNCH

--- APMrover2/mode_launch.cpp
+++ APMrover2/mode_launch.cpp
@@ -1,29 +1,46 @@
 #include "mode.h"
 #include "Rover.h"
 
+/*
+Essentially: On the GCS you enter "LAUNCH MODE" or "LAUNCH CONTROL"
+The craft will set the current BEACHED location as "GPS_HOME" 
+The craft will then wait for stick input
+REVERSE the throttle stick while also input RUDDER direction
+The craft will automaticaly reverse, and then 360 turn in the direction of stick input
+*/
+
+// CLASS LEVEL steering and throttle variables
+float desired_steering, desired_throttle;
+
 bool ModeLaunch::_enter()
 {
     // sets current location as home 
     set_home_to_current_location(true);
-        
-    // steering and throttle variabls
-    float desired_steering, desired_throttle;
     
     // wait for reverse command from TX System To start Program
     while (true) {
-    get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle); // read throttle stick
-    if(is_negative(desired_throttle) {
-        return true;
-        break;
-    }
+        get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle); // read throttle stick 
+        if(is_negative(desired_throttle)) {
+            return true;
+        }
     }
 }
 
 void ModeLaunch::update()
 {
-    float throttle_BACK = -40; // creates throttle variable
-    float throttle_TURN = 20; // creates throttle variable
-    float steering = 4500; // creates throttle variable
+    get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle); // read steering
+    float throttle_BACK = -30; // creates throttle variable
+    float throttle_TURN = 12; // creates throttle variable
+    float steering; // creates throttle variable
+    if(is_negative(desired_steering)) {
+         steering = -4500;
+    }
+    else if (!is_negative(desired_steering)) {
+         steering = 4500;
+    }
+    else {
+         steering = 0.0;
+    }
     time_t start, end, elapsed, count; // creates necessary time objects
     
     //boat runs in reverse at first during Launch mode
@@ -48,7 +65,7 @@ void ModeLaunch::update()
         end = time(0);
         count = end - start;
         elapsed = elapsed + count;
-    } // timer system (6 seconds)
+    } // timer system (3 seconds)
     
     control_mode(&mode_manual); //switches to manual control
     
